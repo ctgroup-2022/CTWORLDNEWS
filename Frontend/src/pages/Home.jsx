2d570db445a03b9f29d56bc18b31cf1a9dc8f3f1
CT Group Development <119471072+ctgroup-2022@users.noreply.github.com>
2026-01-06 10:47:39 +0530

    Update PDF files and thumbnail images in Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -9,9 +9,12 @@ import { useTheme } from "../context/ThemeContext";
 import BannerImg from "../assets/Images/Banner.jpeg";
 import pdfFile from "../assets/pdf/1.pdf";
 import pdfFile1 from "../assets/pdf/2.pdf";
-import thumbnailImage from "../assets/Images/1.png";
-import thumbnail1 from "../assets/Images/2.png";
-import thumbnail2 from "../assets/Images/thumb2.png";
+import pdfFile2 from "../assets/pdf/3.pdf";
+import pdfFile3 from "../assets/pdf/4.pdf";
+import thumbnailImage from "../assets/Images/2.png";
+import thumbnail1 from "../assets/Images/3.png";
+import thumbnail2 from "../assets/Images/4.png";
+import thumbnail3 from "../assets/Images/5.png";
 import ReactPaginate from "react-paginate";
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; // Importing icons for previous and next buttons
 import '../index.css';
@@ -45,23 +48,23 @@ function Home({ searchQuery }) {
     },
     {
       imageSrc: thumbnail2,
-      title: "AI Breakthrough: How GPT-5 is Transforming Industries",
+      title: "",
       description:
-        "The next generation of AI models is here. GPT-5 promises faster, more accurate results, revolutionizing multiple industries...",
-      date: "Sep 15, 2024",
-      pdfSrc: pdfFile,
-      categories: ["Tech"],
-      onDownloadFileName: "gpt5-ai-news.pdf",
+        "",
+      date: "September, 2025",
+      pdfSrc: pdfFile2,
+     
+      onDownloadFileName: "3.pdf",
     },
     {
-      imageSrc: thumbnail1,
-      title: "Sustainability in Business: A New Era of Environmental Responsibility",
+      imageSrc: thumbnail3,
+      title: "",
       description:
-        "As the world shifts towards a more sustainable future, businesses are adapting to new environmental regulations and practices...",
-      date: "Oct 1, 2024",
-      pdfSrc: pdfFile,
-      categories: ["Tech", "Business"],
-      onDownloadFileName: "sustainability-in-business.pdf",
+        "",
+      date: "August, 2025",
+      pdfSrc: pdfFile3,
+    
+      onDownloadFileName: "4.pdf",
     },
     {
       imageSrc: thumbnailImage,
